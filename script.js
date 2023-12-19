a5519386f6f84e8559a899973b7a413e6c76641f
Dyson Carter <129555170+DysonCarter@users.noreply.github.com>
2023-12-19 16:18:01 +0000

    Add functionality to Home page buttons

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+//Functions for redirect buttons
 function goToNormal(){
     window.location.href = "normal.html";
+}
+function goToBinomial(){
+    window.location.href = "binomial.html";
+}
+function goToNB(){
+    window.location.href = "nb.html";
+}
+function goToPoisson(){
+    window.location.href = "poisson.html";
+}
+function goToExponential(){
+    window.location.href = "exponential.html";
+}
+function goToGamma(){
+    window.location.href = "gamma.html";
+}
+function goToChi(){
+    window.location.href = "chi.html";
+}
+function goToToT(){
+    window.location.href = "t.html";
 }
