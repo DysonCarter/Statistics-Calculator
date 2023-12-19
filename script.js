4b63ee9ced625b06af4236ffa20177bb20cd88fd
Dyson Carter <129555170+DysonCarter@users.noreply.github.com>
2023-12-19 16:59:14 +0000

    add functionality to binomial page

--- script.js
+++ script.js
@@ -23,3 +23,32 @@ function goToChi(){
 function goToToT(){
     window.location.href = "t.html";
 }
+
+//Functions for calculation
+function binomial(){
+    var x = parseInt(document.getElementById('x').value);
+    var p = parseFloat(document.getElementById('p').value);
+    var n = parseInt(document.getElementById('n').value);
+
+    // Check if the input values are valid
+    if (isNaN(p) || isNaN(n) || p < 0 || p > 1 || n <= 0 || x > n) {
+        alert('Please enter valid values for p and n.');
+        return;
+    }
+
+    var result = ( choose(n,x) * Math.pow(p,x) * Math.pow(1-p, n-x)) //by definition of binomial
+    // Display the result
+    document.getElementById('result').textContent = 'Binomial Distribution: ' + result.toFixed(4);  
+}
+
+//Math Functions
+function choose(n, r) {
+    return factorial(n) / (factorial(r) * factorial(n - r));
+}
+
+function factorial(x) {
+    if (x === 0 || x === 1) {
+        return 1;
+    }
+    return x * factorial(x - 1);
+}
